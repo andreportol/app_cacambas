a12e61e741c0a386942fa95d3bad2fa9fcc32246
andreportol <andreportol@gmail.com>
2024-10-21 09:54:37 -0400

    Corrigindo erro de digitação no settings.py

--- projeto_cacamba/settings.py
+++ projeto_cacamba/settings.py
@@ -17,7 +17,7 @@ from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.paht.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
